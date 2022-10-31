789258d4c9dfcf7ae28bb79caa259b5cc0fec1b3
doch13_ <dohui0922@dimigo.hs.kr>
2022-10-31 12:05:18 +0900

    fix: Thursday Word Spell

--- lib/pages/meal_exception/controller.dart
+++ lib/pages/meal_exception/controller.dart
@@ -23,7 +23,7 @@ extension WeekDayEnumExtension on int {
       case 1: return "mon";
       case 2: return "tue";
       case 3: return "wed";
-      case 4: return "thu";
+      case 4: return "thr";
       case 5: return "fri";
       case 6: return "sat";
       case 7: return "sun";
